341c434591c9971eaa88d30571d423ceb7aa0c40
umaidashraf0 <umaidashraf0@gmail.com>
2020-07-03 01:12:23 +0500

    fjdhjerdf

---
+++ cookbooks/mouzma-express-app/README.md
@@ -0,0 +1,4 @@
+# mouzma-express-app
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/mouzma-express-app/recipes/default.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook:: deploy-application
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+
+package 'npm' do
+	action :install
+end
+
+
+application '/srv/myapp' do
+	git '/srv/myapp' do
+		repository 'https://github.com/mnazir23/conFusionServer.git'
+	end
+	npm_install
+	npm_start
+end
+
+
+#application '/srv/myapp' do
+#  javascript '3'
+#end
+#
+#application '/srv/myapp' do
+#  javascript_service 'main.js'
+#end
+#
+#application '/srv/myapp' do
+#  node_package 'grunt-cli'
+#end
+#
+#application '/srv/myapp' do
+#  npm_start
+#end
